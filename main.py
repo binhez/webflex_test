15da870726c57a83b8e63eb4318654df0da70d74
vttongg <vttongg@gmail.com>
2022-08-23 10:53:22 +0700

    init

--- main.py
+++ main.py
@@ -1,45 +1,55 @@
+import configparser
 from flask import Flask, jsonify, request, send_file
-import pymssql
 import pandas as pd
+from utils.sqlutil import SqlConn
 import datetime
-
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def web_flex_tracking():
     # Use a breakpoint in the code line below to debug your script.
-    name = request.args.get('name')
+    product_name = request.args.get('product')
+    user_name = request.args.get('user')
+    day = request.args.get('date')
 
+    config = configparser.ConfigParser()
+    config.read('appconfig.ini')
+
+    server = config['SQL_CONNECTION']['SERVER']
+    database = config['SQL_CONNECTION']['GVI_DATABASE']
+    username = config['SQL_CONNECTION']['USERNAME']
+    password = config['SQL_CONNECTION']['PASSWORD']
+    driver = config['SQL_CONNECTION']['DRIVER']
+    procedure = config['SQL_CONNECTION']['PROCEDURE']
     # server = "10.200.10.4"
     # database = "hb_GVI_PVS"
     # username = "sa"
     # password = "Energizer1234!"
     # driver = "{ODBC Driver 17 for SQL Server}"
-    # print(server, database, username, password, driver)
-    # with SqlConn(server, username, password, driver) as gvi_sql:
-    #     # input_user = "NULL"
-    #     input_product = "'PVSJAX20210730-92337'"
-    #     # input_date = "NULL"
-    #     sqlquery = f"""\
-    #         EXEC [{database}].[dbo].[WebflexTrackingUser] @product = ?;
-    #         """
-    #     print(sqlquery)
-    #     # values = (input_user, input_product, input_date)
-    #     values = (input_product)
-    #     logging.info("CALL SP FROM DATABASE - SQL QUERY:" + str(sqlquery) + " VALUE: " + str(values))
-    #     query_result = gvi_sql.excute_sp(database, sqlquery, values)
-    #     logging.info(query_result)
-    #     data = query_result[0].fetchall()
-    #     print(data)
+    print(server, database, username, password, driver)
+    with SqlConn(server, username, password, driver) as gvi_sql:
+        input_user = user_name
+        input_product = product_name
+        input_date = day
+        print(input_product)
+        sqlquery = f"""\
+            SET NOCOUNT ON;
+            EXEC [{database}].[dbo].[{procedure}] @user = ?, @product = ?, @date = ?;
+            """
+        print(sqlquery)
+        values = (input_user, input_product, input_date)
+        # values = (input_product)
+        query_result = gvi_sql.excute_sp(database, sqlquery, values)
+        print(query_result)
 
-    conn = pymssql.connect(server='10.200.10.4', user='sa', password='Energizer1234!', database='hb_GVI_PVS')
-    cursor = conn.cursor()
-    cursor.execute("set nocount on; EXEC WebflexTrackingUser @user = 'GVIAdmin'")
+    # conn = pymssql.connect(server='10.200.10.4', user='sa', password='Energizer1234!', database='hb_GVI_PVS')
+    # cursor = conn.cursor()
+    # cursor.execute("set nocount on; EXEC WebflexTrackingUser @user = 'GVIAdmin'")
     # cursor.execute("SELECT TOP 50 * FROM Product")
-    values = []
-    for row in cursor.fetchall():
-        values.append(list(row))
+    values = query_result
+    # for row in cursor.fetchall():
+    #     values.append(list(row))
 
     df = pd.DataFrame(values, index=None, columns=['Id', 'MasterItemNumber', 'ProductType',
                                     'SpinMeExportedDay', 'CaseSellable', 'DateVerified',
