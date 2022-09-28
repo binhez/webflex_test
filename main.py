36078757f73834ed26bd198ccb2ec1f739067f60
binhez <binhez99@gmail.com>
2022-09-28 14:23:01 +0700

    make it better

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import configparser
 from flask import Flask, jsonify, request, send_file
-import pandas as pd
-from utils.sqlutil import SqlConn
-import datetime
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home_view():
     return "<h1>Hello World</h1>"
-
-# web_flex_tracking()
